86e62067cb08d4a4526a5b73a1312ec56ae256a7
Akhil <108akhilraj@gmail.com>
2022-07-01 20:31:14 +0530

    task 3

--- sextant/src/components/Endpoints/Ip.js
+++ sextant/src/components/Endpoints/Ip.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Ip = () => {
+const Ip = ({IpType}) => {
+
+  const [data6, setData6]=useState();
+  const [data4, setData4]=useState();
+
+  const getData= ()=>{
+    fetch('https://api64.ipify.org?format=json')
+    .then(results => results.json())
+    .then(data => {
+      setData4(data.ip)
+    },
+    (error)=>{
+      setData4("NO IP4 FOUND")
+    }
+    );
+  }
+
+  const getData6= ()=>{
+    fetch('https://api.ipify.org?format=json')
+    .then(results => results.json())
+    .then(data => {
+      setData6(data.ip)
+    },
+    (error)=>{
+      setData6("NO IP6 FOUND")
+    }
+    );
+  }
+
+  useEffect(()=>{
+      getData();
+      getData6();
+
+  },[])
+
+
   return (
-    <div>Ip</div>
+    <div>
+
+     {
+      (IpType===4) && <h1 className='font-bold text-center text-xl'>{data4}</h1>
+     }
+     {
+      (IpType===6) && <h1 className='font-bold text-center text-xl'>{data6}</h1>
+     }
+
+    </div>
   )
 }
 
--- sextant/src/components/MainLayout.js
+++ sextant/src/components/MainLayout.js
@@ -10,12 +10,34 @@ import Dp4 from './Endpoints/Dp4'
 const MainLayout = () => {
 
     const [dataPoint,setDataPoint]=useState(1);
+    const [IpType,setIpType]=useState(4);
+    const [isClicked,setIsClicked] =useState(false);
+
+    const onClickHandler4=()=>{
+            setIpType(4);
+            setIsClicked(true);
+    }
+
+    const onClickHandler6=()=>{
+            setIpType(6);
+            setIsClicked(true);
+
+    }
+
 
   return (
     <div>
         {(dataPoint===1) && 
             <Container dataPoint={dataPoint} setDataPoint={setDataPoint}>
-                {<Ip/>}
+                    <div className='mt-4 space-x-5 space-y-5 sm:mt-6 p-3 '>
+                        <button onClick={onClickHandler4} className='inline-block px-5 py-3 ml-5 rounded-lg transform transition bg-indigo-500 hover:bg-indigo-400 hover:-translate-y-0.5 focus:ring-indigo-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-indigo-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base'>Get my Ip4 Address</button>
+                        <button onClick={onClickHandler6} className='inline-block px-5 py-3 rounded-lg transform transition bg-indigo-500 hover:bg-indigo-400 hover:-translate-y-0.5 focus:ring-indigo-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-indigo-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base'>Get my Ip6 Address</button>
+                    </div>
+                <div className='flex justify-center sm:justify-start sm:ml-10 mt-10 mr-10'>
+                        {
+                        (isClicked) && <Ip IpType={IpType} />
+                        }
+                </div>
             </Container>
         }
         {(dataPoint===2) && 
